1bb659cc68ca5cf731d6db38c8377701da3597fa
Srujana Kovuri <135617161+ksrujana04@users.noreply.github.com>
2024-01-21 16:08:54 +0530

    Add files via upload

--- discussions.js
+++ discussions.js
@@ -1,42 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const discussionSchema = new mongoose.Schema({
+    message:{
+        type: String
+    }
+})
 
-const app = express();
-const port = 3000;
+const Discussion = mongoose.model('Discussions', discussionSchema)
 
-app.use(express.urlencoded({ extended: true }));
-
-mongoose.connect('mongodb://localhost:27017/Harmonicheaven', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Database Connection Done");
-}).catch((err) => {
-    console.log("Something went wrong", err);
-});
-
-//const User = require("./model/discussions");
-const Discussion = require("./model/discussions");
-
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-    res.render("discussions");
-});
-
-app.post("/", (req, res) => {
-    const user = new Discussion(req.body);
-    user.save()
-        .then(() => {
-            console.log("User saved successfully");
-            res.redirect("/"); // Redirect to the home page or wherever appropriate
-        })
-        .catch((err) => {
-            console.log("Error saving user", err);
-            res.status(500).send("Internal Server Error");
-        });
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+module.exports = Discussion
--- userlogin.js
+++ userlogin.js
@@ -1,66 +1,47 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const loginSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        //required: true
+    },
+    mobileno: {
+        type:String,
+        //required: true
+    },
+    emailid: {
+        type:String,
+        //required: true
+    },
+    dob: {
+        type:String,
+        //required: true
+    },
+    password: {
+        type: String,
+        //required: true
+    },
+    state: {
+        type: String,
+        //required: true
+    },
+    district: {
+        type: String,
+        //required: true
+    },
+    city: {
+        type: String,
+        //required: true
+    },
+    locality: {
+        type: String,
+        //required: true
+    },
+    address: {
+        type: String,
+        //required: true
+    }
+})
 
-const app = express();
-const port = 4000;
+const User = mongoose.model('Users', loginSchema)
 
-app.use(express.urlencoded({ extended: true }));
-
-mongoose.connect('mongodb://localhost:27017/Harmonicheaven', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Database Connection Done");
-}).catch((err) => {
-    console.log("Something went wrong", err);
-});
-
-const User = require("./model/userlogin");
-//const Discussion = require("./model/userlogin");
-
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-    res.render("userlogin");
-});
-
-app.get("/usersignup", (req, res) => {
-  res.render("usersignup");
-});
-
-app.post("/", async (req, res) => {
-    try {
-        // check if the user exists
-        const user = await User.findOne({ emailid: req.body.emailid });
-        if (user) {
-          //check if password matches
-          const result = req.body.password === user.password;
-          if (result) {
-            res.render("home");
-          } else {
-            res.status(400).json({ error: "password doesn't match" });
-          }
-        } else {
-          res.status(400).json({ error: "User doesn't exist" });
-        }
-      } catch (error) {
-        res.status(400).json({ error });
-      }
-});
-
-app.post("/usersignup", (req, res) => {
-  const user = new User(req.body);
-  user.save()
-      .then(() => {
-          console.log("User saved successfully");
-          res.render("home"); // Redirect to the home page or wherever appropriate
-      })
-      .catch((err) => {
-          console.log("Error saving user", err);
-          res.status(500).send("Internal Server Error");
-      });
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+module.exports = User
